1bc2514b15086d5a649027e84f060fa3bbb3b910
Tomcat <polpware@gmail.com>
2025-07-17 10:52:36 -0400

    Update toolchain

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const { src, dest, parallel } = require('gulp');
-const typedoc = require("gulp-typedoc");
 const bumpversion = require('gulp-bump');
 
 // `fs` is used instead of require to prevent caching in watch (require caches)
@@ -11,36 +10,15 @@ function getVersion() {
     });
 };
 
-function doc() {
-    return src(["projects/polpware/fe-utilities/src/lib/**/*.ts"])
-        .pipe(typedoc({
-            name: "Polpware typescript utilities (13.0.0)",            
-            out: "docs/",            
-            
-            module: "commonjs",
-            target: "es5",
-
-            exclude: "projects/polpware/fe-utilities/src/lib/**/*.spec.ts",            
-
-            experimentalDecorators: true,
-            excludePrivate: true,
-            excludeExternals: true,
-
-            "lib": [
-                 "lib.dom.d.ts",
-                "lib.es2015.d.ts",                
-                "lib.es2016.d.ts"                
-            ]
-            
-        }));
-}
-
 function bump() {
 
     const newVer = getVersion().trim();
 
-// bump versions on package/bower/manifest
-    return src(['./package.json'])
+    // bump versions on package/bower/manifest
+    return src([
+        './package.json',
+        './projects/polpware/fe-utilities/package.json'        
+    ])
         .pipe(bumpversion({
             version: newVer
         }))
@@ -50,5 +28,4 @@ function bump() {
 }
 
 exports.bump = bump;
-exports.doc = doc;
-exports.default = doc;
+exports.default = bump;
